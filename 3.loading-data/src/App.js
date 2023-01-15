7833bfd8c8e974384e3ccfce9ec60f64d4d4fe81
Nabil Shahid <nabilmd901@gmail.com>
2023-01-15 13:36:48 +0600

    loaded hex values from api

--- 3.loading-data/src/App.js
+++ 3.loading-data/src/App.js
@@ -1,26 +1,27 @@
 import { useState, useEffect } from "react";
-import { csv, csvFormat } from "d3";
+import { csv } from "d3";
 import "./App.css";
 
 const csvUrl =
   "https://gist.githubusercontent.com/nabil404/48e402a70303bb711f0fbbf1849f099f/raw/css_named_colors%2520.csv";
 
-const message = (data) => {
-  let message = "";
-  message += Math.round(csvFormat(data).length / 1024) + " kb\n";
-  message += data.length + " rows\n";
-  message += data.columns.length + " columns";
-  return message;
-};
-
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    csv(csvUrl).then((data) => setData(message(data)));
+    csv(csvUrl).then((data) => setData(data));
   }, []);
 
-  return <pre>Data is {data ? data : "loading..."}</pre>;
+  if (!data) {
+    return <pre>Loading</pre>;
+  }
+
+  return data.map((d) => (
+    <div
+      key={Math.random()}
+      style={{ background: d["RGB hex value"], width: "100vw", height: "50px" }}
+    ></div>
+  ));
 }
 
 export default App;
